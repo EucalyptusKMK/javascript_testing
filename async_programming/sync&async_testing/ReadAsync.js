cc64f5f459771039f5bd8bedc0ed6746de9ff442
kaungmyatkyaw <kaungmyatkyaweucalyptus2003@gmail.com>
2024-05-23 16:00:41 +0630

    changing the testing

--- async_programming/sync&async_testing/ReadAsync.js
+++ async_programming/sync&async_testing/ReadAsync.js
@@ -3,8 +3,7 @@ let start = new Date();
 
 let p1 = fs.promises.readFile("text.txt", "utf8");
 let p2 = fs.promises.readFile("text2.txt", "utf8");
-Promise
-        .all([p1, p2])
+Promise.all([p1, p2])
         .then(data => {
             let end = new Date();
             let time = end - start;
